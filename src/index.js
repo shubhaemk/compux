276f0540845a951b6ff2a1a76df8ee305cb05430
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-23 18:40:02 +0530

    added - check directory function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@
  * componentDirectory - Represents component directory in src
  * argv - Array of passed options
  */
-
+import "regenerator-runtime/runtime";
+import { access, stat } from 'fs';
 import { resolve } from 'path';
 const srcDirectory = resolve(process.cwd(),'src');
-const componentDirectory = resolve(srcDirectory,'components');
+const componentsDirectory = resolve(srcDirectory,'components');
 const [,,...argv] = process.argv;
 
 let componentName;
@@ -37,16 +38,69 @@ const getComponentName = name => {
 }
 
 const checkDirectory = directory => {
-    
+    return new Promise();
 }
 
-const _main = (argv) => {
+const checkExist = async (directory) => {
+    return new Promise((resolve,reject)=>{
+        access(directory,err => {
+            if(err){
+                reject(false);
+            }
+            try{
+                await checkDirectory(directory);
+                resolve(true);
+            }catch(err){
+                reject(false);
+            }
+        });
+    });
+}
+
+const createDirectory = directory => {
+    console.log(`Creating ${directory} ...`)
+}
+
+const _main = async (argv) => {
     if(argv.length > 1){
         console.log('Error! I dont accept more than 1 parameter!')
     }else{
         componentName = argv[0];
         if(checkComponentName(componentName)){
             componentNameCc = getComponentName(componentName);
+
+            try{
+                let srcDirExist = await checkExist(srcDirectory);
+                if(srcDirExist){
+                    console.log('/src does exist!');
+                }
+            }catch(err){
+                console.log('/src does not exist!');
+                console.log('Creating /src for you!');
+                createDirectory(srcDirectory);
+            }
+            
+            try {
+                let componentsDirExist = await checkExist(componentsDirectory);
+                if (componentsDirExist) {
+                    console.log('/component does exist!');
+                }
+            } catch (err) {
+                console.log('/components does not exist!');
+                console.log('Creating /components for you!');
+                createDirectory(componentsDirectory);
+            }
+
+            try {
+                let componentDirExist = await checkExist(componentsDirectory,componentName);
+                if (componentDirExist) {
+                    console.log(`${componentName} already exists!`);
+                }
+            } catch (err) {
+                console.log(`/${componentName} does not exist!`);
+                console.log(`Creating /${componentName} for you!`);
+                createDirectory(resolve(componentsDirectory,componentName));
+            }
             
         }else{
             console.log(`Use component name of format 'component-name'`)
