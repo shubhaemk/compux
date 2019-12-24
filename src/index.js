fe055c9669d42b46562ce0e02d881fc6a3a96ba8
Shubham Kamath <dev.shubhaem@gmail.com>
2019-12-24 15:43:27 +0530

    fixed - unhandledPromiseRejection

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@
  * argv - Array of passed options
  */
 import "regenerator-runtime/runtime";
-import { access, stat } from 'fs';
+import { access, stat, mkdir } from 'fs';
 import { resolve } from 'path';
 const srcDirectory = resolve(process.cwd(),'src');
 const componentsDirectory = resolve(srcDirectory,'components');
 const [,,...argv] = process.argv;
 
 let componentName;
+let componentDirectory;
 let componentNameCc;
 
-//need fix : should only work for abc-abc and not abc-abc-abc...
 const checkComponentName = name => {
     //RegExp('[a-zA-Z]+[-][a-zA-Z]+').test(name) doesn't work might do manual search!
     
@@ -37,88 +37,114 @@ const getComponentName = name => {
     return `${first}${second}`
 }
 
-const checkDirectory = directory => {
+const isDirectory = directory => {
     return new Promise((resolve,reject)=>{
         stat(directory,(err,stats)=>{
             if (err){
-                reject(false);
-                return;
+                reject({
+                    code: 4002,
+                    file: directory
+                });
             }
             if (stats.isDirectory()){
                 resolve(true);
-                return;
+            }else{
+                reject({
+                    code: 4001,
+                    file: directory
+                });
             }
-            reject(false);
             return;
         });
     });
 };
 
-const checkExist = async (directory) => {
+const fileExists = directory => {
     return new Promise((resolve,reject)=>{
         access(directory,err => {
             if(err){
-                reject(false);
-            }
-            try{
-                let directoryExist = await checkDirectory(directory);
-                if (directoryExist) {
-                    resolve(true);
-                }
-            }catch(error){
-                console.log(`
-                    -------------${error}-----------
-                `)
-                resolve(false);
+                reject({
+                    code: 4000,
+                    file: directory
+                });
+            }else{
+                resolve(true);
             }
         });
     });
 };
 
 const createDirectory = directory => {
-    console.log(`Creating ${directory} ...`)
-}
+    return new Promise((resolve,reject) => {
+        mkdir(directory,err => {
+            if(err){
+                reject({
+                    code: 4003,
+                    file: directory
+                });
+            }else{
+                resolve(true);
+            }
+        })
+    });
+};
 
-const _main = async (argv) => {
+
+
+
+
+const _main = (argv) => {
     if(argv.length > 1){
         console.log('Error! I dont accept more than 1 parameter!')
     }else{
         componentName = argv[0];
         if(checkComponentName(componentName)){
             componentNameCc = getComponentName(componentName);
+            componentDirectory = resolve(componentsDirectory,componentName);
 
-            try{
-                let srcDirExist = await checkExist(srcDirectory);
-                if(srcDirExist){
-                    console.log('/src does exist!');
+            const listDir = [
+                {
+                    name: srcDirectory,
+                    create: false
+                },
+                {
+                    name: componentsDirectory,
+                    create: false
+                },
+                {
+                    name: componentDirectory,
+                    create: true
                 }
-            }catch(err){
-                console.log('/src does not exist!');
-                console.log('Creating /src for you!');
-                createDirectory(srcDirectory);
-            }
-            
-            try {
-                let componentsDirExist = await checkExist(componentsDirectory);
-                if (componentsDirExist) {
-                    console.log('/component does exist!');
-                }
-            } catch (err) {
-                console.log('/components does not exist!');
-                console.log('Creating /components for you!');
-                createDirectory(componentsDirectory);
-            }
+            ];
 
-            /* try {
-                let componentDirExist = await checkExist(componentsDirectory,componentName);
-                if (componentDirExist) {
-                    console.log(`${componentName} already exists!`);
+            fileExists(componentsDirectory)
+            .then(srcExist => {
+                if(srcExist){
+                    console.log('/src exist!');
                 }
-            } catch (err) {
-                console.log(`/${componentName} does not exist!`);
-                console.log(`Creating /${componentName} for you!`);
-                createDirectory(resolve(componentsDirectory,componentName));
-            } */
+                return isDirectory(componentsDirectory);
+            })
+            .then(isDir => {
+                if (isDir) {
+                    console.log('Is directory');
+                }
+            })
+            .catch(error => {
+                if(error.code === 4000){
+                    return createDirectory(componentsDirectory);
+                }
+                if(error.code === 4001){
+                    console.log('Cant go any further than this');
+                }
+            })
+            .then(success => {
+                if(success){
+                    console.log('Created new dir!')
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
             
         }else{
             console.log(`Use component name of format 'component-name'`)
