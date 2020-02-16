51dfb43b4add2be39f89cb58282a9a814406354b
Shubham Kamath <dev.shubhaem@gmail.com>
2020-02-16 20:25:09 +0530

    changed error object to meet messages requirement

--- src/modules/fs/directory.js
+++ src/modules/fs/directory.js
@@ -1,52 +1,50 @@
 import { access, stat, mkdir } from 'fs';
 
-export const isDirectory = file => (
-    new Promise((resolve, reject) => {
-        stat(file, (error, stats) => {
-            if (error) {
-                reject({
-                    code: 4002,
-                    file: file
-                });
-            }
-            if (stats.isDirectory()) {
-                resolve();
-            } else {
-                reject({
-                    code: 4001,
-                    file: file
-                });
-            }
+export const isDirectory = file =>
+  new Promise((resolve, reject) => {
+    stat(file, (error, stats) => {
+      if (error) {
+        reject({
+          code: 4002,
+          reason: file
         });
-    })
-)
-
-export const isAccessible = file => (
-    new Promise((resolve, reject) => {
-        access(file, error => {
-            if (error) {
-                reject({
-                    code: 4000,
-                    file: file
-                });
-            } else {
-                resolve();
-            }
+      }
+      if (stats.isDirectory()) {
+        resolve();
+      } else {
+        reject({
+          code: 4001,
+          reason: file
         });
-    })
-)
+      }
+    });
+  });
 
-export const createDirectory = file => (
-    new Promise((resolve, reject) => {
-        mkdir(file, { recursive: true }, error => {
-            if (error) {
-                reject({
-                    code: 4003,
-                    file: file
-                });
-            } else {
-                resolve();
-            }
-        })
-    })
-)
+export const isAccessible = file =>
+  new Promise((resolve, reject) => {
+    access(file, error => {
+      if (error) {
+        //should not be handled
+        reject({
+          code: 4000,
+          reason: file
+        });
+      } else {
+        resolve();
+      }
+    });
+  });
+
+export const createDirectory = file =>
+  new Promise((resolve, reject) => {
+    mkdir(file, { recursive: true }, error => {
+      if (error) {
+        reject({
+          code: 4003,
+          reason: file
+        });
+      } else {
+        resolve();
+      }
+    });
+  });
