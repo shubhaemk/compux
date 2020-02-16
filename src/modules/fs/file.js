2ddf745d45d4c4f08356ef42593d4d9712ca3620
Shubham Kamath <dev.shubhaem@gmail.com>
2020-02-16 20:25:23 +0530

    added file creation function

--- src/modules/fs/file.js
+++ src/modules/fs/file.js
@@ -1,8 +1,23 @@
 import { writeFile } from 'fs';
+import { join } from 'path';
+import { getClassTemplate, getFunctionTemplate } from '../string/template';
+import { componentTypeResolve } from '../string/name';
 
-
-const componentFileCreate = path => (
-    new Promise((resolve, reject) => {
-
-    })
-);
+export const componentFileCreate = (path, componentNameCc, componentType) => {
+  const file = join(path, `${componentNameCc}.jsx`);
+  const template =
+    componentTypeResolve(componentType) === 'class'
+      ? getClassTemplate(componentNameCc)
+      : getFunctionTemplate(componentNameCc);
+  return new Promise((resolve, reject) => {
+    writeFile(file, template, error => {
+      if (error) {
+        reject({
+          code: 3000,
+          reason: file
+        });
+      }
+      resolve();
+    });
+  });
+};
